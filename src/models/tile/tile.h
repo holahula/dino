71b12f87819e5dd20b9aa813deadc3442bb97441
Arjun Bhushan <arjun.bhushan2@gmail.com>
2019-07-05 19:56:29 -0400

    Removed 'using namespace std' in the Tile header

--- src/models/tile/tile.h
+++ src/models/tile/tile.h
@@ -2,14 +2,13 @@
 #define TILE_H_
 
 #include <utility>
-using namespace std;
 
 class Tile {
     int x;
     int y;
     public:
         Tile(int x, int y);
-        pair<int,int> location();
+        std::pair<int,int> location();
         virtual ~Tile() = 0;
 };
 
