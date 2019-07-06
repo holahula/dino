730de3c6e11333be154069b25c686d0a73d4cf40
Austin Jiang <austin.jiang@uwaterloo.ca>
2019-07-06 17:53:15 -0400

    allow game state to check tower range for map

--- src/models/tower/tower.cc
+++ src/models/tower/tower.cc
@@ -5,7 +5,12 @@ using namespace std;
 Tower::Tower() : range(3) {}
 Tower::~Tower() {}
 
+int Tower::getRange() const {
+    return range;
+}
+
 void Tower::attack() {
     notifyObservers(this);
 }
+
 
