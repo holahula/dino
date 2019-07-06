e69860a618cc9bba694e360883c883959eadb745
Arjun Bhushan <arjun.bhushan2@gmail.com>
2019-07-06 03:29:31 -0400

    Added barebones map

--- src/models/map/map.h
+++ src/models/map/map.h
@@ -1,9 +1,24 @@
 #ifndef MAP_H_
 #define MAP_H_
 
+#include "../tile/tile.h"
+#include "../tile/path/pathTile.h"
+#include "../enemy/enemy.h"
+#include <vector>
+#include <utility>
+#include <iostream>
 
 class Map {
-    // TODO
+    std::vector<std::vector<Tile*> > map;
+    std::vector<PathTile*> path;
+    std::vector<std::pair<int,int> > getPath(std::pair<int,int> from, std::pair<int,int> to);
+    public:
+        Map();
+        ~Map();
+
+        // returns vector of dead enemies and completed enemies
+        std::pair<std::vector<Enemy*>, std::vector<Enemy*> > nextFrame();
+        friend std::ostream& operator<<(std::ostream&, Map &);
 };
 
 #endif
