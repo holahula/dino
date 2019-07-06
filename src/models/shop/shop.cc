3e08805b0f2fb583facf31470a6acce500121fef
Jasmine Ou <jasmineou99@gmail.com>
2019-07-06 02:36:13 -0400

    Add state and shop (#6)

---
+++ src/models/shop/shop.cc
@@ -0,0 +1,9 @@
+#include "shop.h"
+
+using namespace std;
+
+pair<bool, Tower> Shop::buy(Tower tower) {
+}
+
+void Shop::sell(Tower tower) {
+}
--- src/models/state/state.h
+++ src/models/state/state.h
@@ -1,9 +1,23 @@
 #ifndef STATE_H_
 #define STATE_H_
 
+#include <vector>
+#include "../enemy/enemy.h"
+#include "../tower/tower.h"
 
 class State {
-    // TODO
+  int money;
+  int hp;
+  std::vector<Tower*> towers;
+  std::vector<Enemy*> enemies;
+
+ public:
+  State(int money = 100, int HP = 100);
+  ~State();
+  void earn(int);
+  void spend(int);
+  void loseHP(int);
+  void addTower(Tower tower);
 };
 
 #endif
