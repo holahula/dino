30b06df08fd17bcb1f1d43fd96a66b0917ecaf2e
Austin Jiang <austin.jiang@uwaterloo.ca>
2019-07-06 00:04:01 -0400

    fix tower merge conflicts

--- src/models/tower/tower.h
+++ src/models/tower/tower.h
@@ -1,11 +1,18 @@
 #ifndef TOWER_H_
 #define TOWER_H_
 
+#include "./../subject/subject.h"
 
-class Tower {
+class Tower : public Subject {
+    private:
+        int cost;
+
     public:
         Tower();
+
         virtual char getType() = 0;
+        virtual void attack() = 0;
+
         ~Tower();
 };
 
