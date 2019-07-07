d41e188df28ce2af97d085761f729e6908973805
Austin Jiang <austin.jiang@uwaterloo.ca>
2019-07-07 03:08:15 -0400

    update main harness to capture game functionality

--- src/main.cc
+++ src/main.cc
@@ -1,42 +1,71 @@
-#include <iostream>
-#include <sstream>
-#include <string>
 #include "models/enemy/enemy.h"
 #include "models/state/state.h"
 #include "models/tower/tower.h"
 #include "models/map/map.h"
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
 using namespace std;
 
 int main() {
     stringstream ss;
-    State state;
+    State * game;
 
     char cmd, cmd2;
-    int row, col;
+    int x, y;
 
     while (cin >> cmd && cmd != 'q') {
         switch (cmd) {
             // Start a new game
             case 'n':
+                game = new State();
                 break;
             // Buy a tower
             case 'b':
-                cin >> cmd2 >> row >> col;
+                cin >> cmd2 >> x >> y;
                 switch (cmd2) {
                     // damage tower
                     case 'd':
+                        if(!game->buyTower(cmd2, x, y)){
+                            cout << "Invalid Purchase! Tower @ (" << x << ", " << y << ") could not be bought" << endl;
+                        } else {
+                            cout << "Purchase successful!" << endl;
+                        }
                         break;
                     // freeze tower
                     case 'f':
+                        if(!game->buyTower(cmd2, x, y)){
+                            cout << "Invalid Purchase! Tower @ (" << x << ", " << y << ") could not be bought" << endl;
+                        } else {
+                            cout << "Purchase successful!" << endl;
+                        }
                         break;
                     // money tower
                     case 'm':
+                        if(!game->buyTower(cmd2, x, y)){
+                            cout << "Invalid Purchase! Tower @ (" << x << ", " << y << ") could not be bought" << endl;
+                        } else {
+                            cout << "Purchase successful!" << endl;
+                        }
                         break;
                 }
                 break;
-                // Next turn
-            case 't':
+            // Upgrade a tower
+            case 'u':
+                cin >> x >> y;
+                if(!game->upgradeTower(x, y)){
+                    cout << "Invalid Upgrade! Tower @ (" << x << ", " << y << ") could not be upgraded" << endl;
+                } else {
+                    cout << "Upgrade successful!" << endl;
+                }
                 break;
+            // Start Next round
+            case 'r':
+                game->startRound();
+                break;
+
             default:
                 cout << "Invalid command: " << cmd << endl;
                 break;
