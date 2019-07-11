b6328908a3ecdaa6904f7fc66a59d416343ca683
Jasmine Ou <jasmineou99@gmail.com>
2019-07-11 16:31:07 -0400

    Add tower drag and drop

--- src/models/view/tileView/tileView.cc
+++ src/models/view/tileView/tileView.cc
@@ -4,6 +4,9 @@ using namespace std;
 
 TileView::TileView(Tile *t) : Gtk::Label("\t\t\n\t\t\n") {
 	tile = t;
+	type = t->getType();
+	row = t->location().second;
+	col = t->location().first;
 }
 
 TileView::~TileView() {}
