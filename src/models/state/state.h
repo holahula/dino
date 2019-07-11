cc0e188b15e59c07609d76004e95c7742a38df97
Arjun Bhushan <arjun.bhushan2@gmail.com>
2019-07-11 18:42:05 -0400

    Revert "Jasmine/ui" (#33)

--- src/models/state/state.h
+++ src/models/state/state.h
@@ -2,12 +2,12 @@
 #define STATE_H_
 
 #include "./../enemy/enemy.h"
-#include "./../map/map.h"
-#include "./../shop/shop.h"
 #include "./../tower/tower.h"
+#include "./../shop/shop.h"
+#include "./../map/map.h"
 
-#include <stack>
 #include <vector>
+#include <stack>
 
 /*
 TODO: money management functions,
@@ -18,52 +18,49 @@ TODO: money management functions,
  */
 
 class State {
-  int hp, money, round;
+    int hp, money, round;
 
-  Shop *shop;
-  Map *map;
+    Shop * shop;
+    Map * map;
 
-  std::vector<Tower *> towers;
-  std::vector<Enemy *> enemies;
-  std::stack<Enemy *> pendingEnemies;
+    std::vector<Tower*> towers;
+    std::vector<Enemy*> enemies;
+    std::stack<Enemy*> pendingEnemies;
 
-  bool surviveDmg(int);
-  void incrementMoney(int);
-  // void spend(int);
+    bool surviveDmg(int);
+    void incrementMoney(int);
+    // void spend(int);
 
-  void addTower(Tower *);
-  void addEnemy(Enemy *);
-  void removeEnemy(Enemy *);
+    void addTower(Tower *);
+    void addEnemy(Enemy *);
+    void removeEnemy(Enemy *);
 
-  int constructEnemies(int);
+    int constructEnemies(int);
 
-  int totalHPLost(std::vector<Enemy *>);
+    int totalHPLost(std::vector<Enemy*>);
 
-  bool moveEnemies(int, int);
+    bool moveEnemies(int, int);
+    
+    bool preFrame(int, int);
 
-  bool preFrame(int, int);
+    void processFrame();
+    void postFrame();
 
-  void processFrame();
-  void postFrame();
+    void getMoneyTowerIncome();
+    void updateState(int, int);
 
-  void getMoneyTowerIncome();
-  void updateState(int, int);
+    public:
+        static const int MAX_ROUND = 10;
 
- public:
-  static const int MAX_ROUND = 10;
+        State();
+        ~State();
 
-  State();
-  ~State();
+        void displayMap();
+        bool buyTower(char, int, int);
+        bool upgradeTower(int, int);
 
-  void displayMap();
-  bool buyTower(char, int, int);
-  bool upgradeTower(int, int);
+        void startRound();
 
-  void startRound();
-  
-  int getMoney();
-  int getHp();
-  int getRound();
 };
 
 #endif
