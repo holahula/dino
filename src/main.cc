f70c8fabfe2e17e59f91db2602197e57f6b93d39
Arjun Bhushan <arjun.bhushan2@gmail.com>
2019-07-12 15:36:41 -0400

    Add sell tower functionality in towers and map (#41)
    * Added totalCost to each tower
    
    * Add sell operation in state, shop, and main
    
    * Added a catch all for tower types
    
    * Now get money back from the store
    
    * Fixed error when selling tower

--- src/main.cc
+++ src/main.cc
@@ -53,6 +53,9 @@ int main() {
               cout << "Money tower purchase successful!" << endl;
             }
             break;
+          default:
+            cout << "Invalid command!" << endl;
+            break;
         }
         break;
       // Upgrade a tower
@@ -63,6 +66,19 @@ int main() {
         } else {
           cout << "Upgrade successful!" << endl;
         }
+        break;
+      // Sell a tower
+      case 's':
+        cin >> x >> y;
+        if (!game->sellTower(x, y)) {
+          cout << "Invalid Sell Operation! Tower @ (" << x << ", " << y << ") could not be sold" << endl;
+        } else {
+          cout << "Sold successfully!" << endl;
+        }
+        break;
+      // Get total money
+      case 'm':
+        cout << "You currently have: $" << game->getMoney() << endl;
         break;
       // Start Next round
       case 'r':
