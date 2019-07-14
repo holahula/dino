6c7297c22d88a6b9aa4a3a59db58edb33ed7c770
Arjun Bhushan <arjun.bhushan2@gmail.com>
2019-07-14 16:35:34 -0400

    Restructured the switch cases to work properly

--- src/main.cc
+++ src/main.cc
@@ -38,82 +38,91 @@ int text_interface() {
   char cmd, cmd2;
   int x, y;
 
-  cout << "welcome to beans, beans, beans" << endl;
+  cout << "welcome to baloon tower defence!" << endl;
 
 	while (cin >> cmd && cmd != 'q') {
 		switch (cmd) {
 			// Start a new game
-			case 'n':
-			game = unique_ptr<State>(new State());
-			cout << "game created" << endl;
-			break;
+			case 'n': {
+				game = unique_ptr<State>(new State());
+				cout << "game created" << endl;
+				break;
+			}
 			// Buy a tower
-			case 'b':
-			cin >> cmd2 >> x >> y;
-			switch (cmd2) {
-				// damage tower
-				case 'd':
-				if (!game->buyTower('D', x, y)) {
-					cout << "Invalid Purchase! Damage tower @ (" << x << ", " << y << ") could not be bought" << endl;
-				} else {
-					cout << "Damage tower purchase successful!" << endl;
+			case 'b': {
+				cin >> cmd2 >> x >> y;
+				switch (cmd2) {
+					// damage tower
+					case 'd': {
+						if (!game->buyTower('D', x, y)) {
+							cout << "Invalid Purchase! Damage tower @ (" << x << ", " << y << ") could not be bought" << endl;
+						} else {
+							cout << "Damage tower purchase successful!" << endl;
+						}
+						break;
+					}
+					// freeze tower
+					case 'f': {
+						if (!game->buyTower('F', x, y)) {
+							cout << "Invalid Purchase! Freeze tower @ (" << x << ", " << y << ") could not be bought" << endl;
+						} else {
+							cout << "Freeze tower purchase successful!" << endl;
+						}
+						break;
+					}
+					// money tower
+					case 'm': {
+						if (!game->buyTower('M', x, y)) {
+							cout << "Invalid Purchase! Money tower @ (" << x << ", " << y << ") could not be bought" << endl;
+						} else {
+							cout << "Money tower purchase successful!" << endl;
+						}
+						break;
+					}
+					default: {
+						cout << "Invalid command!" << endl;
+					}
 				}
-				break;
-				// freeze tower
-				case 'f':
-				if (!game->buyTower('F', x, y)) {
-					cout << "Invalid Purchase! Freeze tower @ (" << x << ", " << y << ") could not be bought" << endl;
-				} else {
-					cout << "Freeze tower purchase successful!" << endl;
-				}
-				break;
-				// money tower
-				case 'm':
-				if (!game->buyTower('M', x, y)) {
-					cout << "Invalid Purchase! Money tower @ (" << x << ", " << y << ") could not be bought" << endl;
-				} else {
-					cout << "Money tower purchase successful!" << endl;
-				}
-				break;
-				default:
-				cout << "Invalid command!" << endl;
 				break;
 			}
-			break;
 			// Upgrade a tower
-			case 'u':
-			cin >> x >> y;
-			if (!game->upgradeTower(x, y)) {
-				cout << "Invalid Upgrade! Tower @ (" << x << ", " << y << ") could not be upgraded" << endl;
-			} else {
-				cout << "Upgrade successful!" << endl;
+			case 'u': {
+				cin >> x >> y;
+				if (!game->upgradeTower(x, y)) {
+					cout << "Invalid Upgrade! Tower @ (" << x << ", " << y << ") could not be upgraded" << endl;
+				} else {
+					cout << "Upgrade successful!" << endl;
+				}
+				break;
 			}
-			break;
 			// Sell a tower
-			case 's':
+			case 's': {
 			cin >> x >> y;
-			if (!game->sellTower(x, y)) {
-				cout << "Invalid Sell Operation! Tower @ (" << x << ", " << y << ") could not be sold" << endl;
-			} else {
-				cout << "Sold successfully!" << endl;
+				if (!game->sellTower(x, y)) {
+					cout << "Invalid Sell Operation! Tower @ (" << x << ", " << y << ") could not be sold" << endl;
+				} else {
+					cout << "Sold successfully!" << endl;
+				}
+				break;
 			}
-			break;
 			// Get total money
-			case 'm':
-			cout << "You currently have: $" << game->getMoney() << endl;
-			break;
+			case 'm': {
+				cout << "You currently have: $" << game->getMoney() << endl;
+				break;
+			}
 			// Start Next round
-			case 'r':
-			game->startRound();
-			break;
+			case 'r': {
+				game->startRound();
+				break;
+			}
 			// Print map
-			case 'p':
-			game->displayMap();
-			break;
-
-			default:
-			cout << "Invalid command: " << cmd << endl;
-			break;
+			case 'p': {
+				game->displayMap();
+				break;
+			}
+			default: {
+				cout << "Invalid command: " << cmd << endl;
+			}
 		}
 	}
 	return 0;
