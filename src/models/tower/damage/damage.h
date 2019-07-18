34d8f75225f915ebd48010fef783f2c95d9d6b06
Jasmine Ou <jasmineou99@gmail.com>
2019-07-17 23:30:37 -0400

    fixed nvi

--- src/models/tower/damage/damage.h
+++ src/models/tower/damage/damage.h
@@ -8,20 +8,19 @@
 class DamageTower : public Tower {
     private:
         int dmg, upCost;
+		int getUpgradeCost_() const override;
+        int getCost_() const override;
+        std::string getDescription_() const override;
+        
+        std::pair<char, int> getType_() const override;
+
+        void upgrade_() override;
 
     public:
         static const int cost = 10;
 
         DamageTower();
         virtual ~DamageTower();
-
-        int getUpgradeCost() const override;
-        int getCost() const override;
-        std::string getDescription() const override;
-        
-        std::pair<char, int> getType() const override;
-
-        void upgrade() override;
 };
 
 #endif
